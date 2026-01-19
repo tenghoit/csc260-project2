5472a5af26c33d7ea3786aeefa1d14143561b6cb
Tenghoit Kouch <tenghoitkouch@gmail.com>
2026-01-19 18:51:09 -0500

    added a search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
@@ -6,28 +6,51 @@ import Table from './Table';
 
 function App() {
   const [grants, setGrants] = useState([]);
+  const [filteredGrants, setFilteredGrants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchField, setSearchField] = useState("");
+
+  const attributes = [
+    "InstCountry",
+    "InstState",
+    "InstCity",
+    "Institution",
+    "ProjectTitle",
+    "ApprovedMatching",
+    "BeginGrant",
+    "EndGrant"
+  ];
 
   useEffect(() => {
     async function fetchGrants() {
         const url = "/NEH2020sGrant_Short.json";
         const response = await fetch(url);
-        if(response.ok){
-          console.log("response ok");
-          
+        if(response.ok){          
           const result = await response.json();
-          console.log(result);
-
           setGrants(result.Grants.Grant);
-          console.log(grants);
+          setFilteredGrants(result.Grants.Grant);
         }
     };
     fetchGrants();
   }, []);
 
+  function handleSubmit(event){
+    event.preventDefault()
+    if(searchField === ""){
+      return;
+    }
+
+    const results = grants.filter((grant) => (
+      grant[searchField].includes(searchTerm)
+    ))
+
+    setFilteredGrants(results);
+  }
+
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -40,9 +63,25 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
+      <header>
+        <h1>NEH 2020's Grants</h1>
       </header>
       <main>
-            <Table grants={grants} />
+            {/* <h2>Filters</h2> */}
+            <form onSubmit={handleSubmit}>
+              <label for="searchTerm">Search for</label>
+              <input type='text' id='searchTerm' name='searchTerm' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} required/>
+              <label for='searchField'>in field</label>
+              <select id='searchField' name='searchField' value={searchField} onChange={(e) => setSearchField(e.target.value)} required>
+                <option value="" autoFocus>Select a field</option>
+                {attributes.map((value) => (
+                  <option value={value}>{value}</option>
+                ))}
+              </select>
+              <button type='submit'>Submit</button>
+            </form>
+            <Table grants={filteredGrants} attributes={attributes} />
       </main>
     </div>
   );
--- src/Table.js
+++ src/Table.js
@@ -1,13 +1,23 @@
+import "./Table.css"
+
 function Table(props){
+
+    function TableHeader(props){
+        return(
+            <tr>
+                {props.attributes.map((attr) => (
+                    <th>{attr}</th>
+                ))}
+            </tr>
+        )
+    }
 
     function TableRow(props){
         return(
             <tr key={props.index}>
-                <td>{props.grant.Institution}</td>
-                <td>{props.grant.ProjectTitle}</td>
-                <td>{props.grant.ApprovedMatching}</td>
-                <td>{props.grant.BeginGrant}</td>
-                <td>{props.grant.EndGrant}</td>
+                {props.attributes.map((attr) => (
+                    <td>{props.grant[attr]}</td>
+                ))}
             </tr>
         )
     }
@@ -16,17 +26,18 @@ function Table(props){
     return (
         <table>
             <thead>
-                <tr>
+                <TableHeader attributes={props.attributes} />
+                {/* <tr>
                     <th>Institution</th>
                     <th>Project Title</th>
                     <th>Approved Matching</th>
                     <th>Begin Grant</th>
                     <th>End Grant</th>
-                </tr>
+                </tr> */}
             </thead>
             <tbody>
                 {props.grants.map((grant, index) => (
-                    <TableRow key={index} grant={grant} />
+                    <TableRow key={index} grant={grant} attributes={props.attributes} />
                 ))}
             </tbody>
         </table>
