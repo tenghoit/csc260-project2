703b70bd04c08a42ac16ca0c74f9bb56b6a6109f
Tenghoit Kouch <tenghoitkouch@gmail.com>
2026-01-20 12:22:22 -0500

    working on sorting

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import Table from './Table';
 function App() {
   const [grants, setGrants] = useState([]);
   const [filteredGrants, setFilteredGrants] = useState([]);
+  const [sortedGrants, setSortedGrants] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchField, setSearchField] = useState("");
+  const [sortField, setSortField] = useState("");
+
 
   const attributes = [
     "InstCountry",
@@ -21,6 +24,8 @@ function App() {
     "EndGrant"
   ];
 
+  let sortableAttributes = [];
+
   useEffect(() => {
     async function fetchGrants() {
         const url = "/NEH2020sGrant_Short.json";
@@ -29,22 +34,50 @@ function App() {
           const result = await response.json();
           setGrants(result.Grants.Grant);
           setFilteredGrants(result.Grants.Grant);
+          setSortedGrants(result.Grants.Grant);
+          getSortableAttributes(result.Grants.Grant);
         }
     };
     fetchGrants();
   }, []);
 
-  function handleSubmit(event){
+  
+  function getSortableAttributes(grants){
+    sortableAttributes = attributes
+    return
+
+    for (const attr of attributes) {
+      console.log(`${attr} : ${grants[0][attr]} : ${typeof grants[0][attr]}`);
+      
+      if(typeof grants[0][attr] === "string"){
+        sortableAttributes.push(attr)
+      }
+    }
+    console.log(sortableAttributes);
+  }
+
+  function handleSearch(event){
     event.preventDefault()
     if(searchField === ""){
       return;
     }
 
     const results = grants.filter((grant) => (
-      grant[searchField].includes(searchTerm)
+      grant[searchField].toLowerCase().includes(searchTerm.toLowerCase())
     ))
 
     setFilteredGrants(results);
+  }
+
+  function handleSort(event){
+    event.preventDefault()
+    if(sortField === ""){
+      return;
+    }
+    
+    let result = [...filteredGrants]
+    result = result.sort((a, b) => a[sortField] - b[sortField])
+    setSortedGrants(result)
   }
 
 
@@ -69,19 +102,29 @@ function App() {
       </header>
       <main>
             {/* <h2>Filters</h2> */}
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSearch}>
               <label for="searchTerm">Search for</label>
               <input type='text' id='searchTerm' name='searchTerm' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} required/>
               <label for='searchField'>in field</label>
               <select id='searchField' name='searchField' value={searchField} onChange={(e) => setSearchField(e.target.value)} required>
                 <option value="" autoFocus>Select a field</option>
-                {attributes.map((value) => (
-                  <option value={value}>{value}</option>
-                ))}
+                {attributes.map(attr => 
+                  <option value={attr}>{attr}</option>
+                )}
               </select>
-              <button type='submit'>Submit</button>
+              <button type='submit'>Search</button>
             </form>
-            <Table grants={filteredGrants} attributes={attributes} />
+            <form onSubmit={handleSort}>
+              <label for="sortField">Sort by:</label>
+              <select id="sortField" name='sortField' value={sortField} onChange={e => setSortField(e.target.value)} required>
+                <option value="" autoFocus>Select a field</option>
+                {sortableAttributes.map(attr =>
+                  <option value={attr}>{attr}</option>
+                )}
+              </select>
+              <button type='submit'>Sort</button>
+            </form>
+            <Table grants={sortedGrants} attributes={attributes} />
       </main>
     </div>
   );
